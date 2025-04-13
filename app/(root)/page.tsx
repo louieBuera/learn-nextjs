8d406fd27f78a023c532098089f7ac01024e4f5b
louieBuera <isaac.277.elifer@gmail.com>
2025-04-13 23:20:20 +0800

    Revert to tailwind v3.* for compatibility with tutorial

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
 
   return (
     <>
-      <h1 className="text-2xl">Welcome to Next.js</h1>
+      <h1 className="heading">Welcome to Next.js</h1>
     </>
   );
 }
