7f7f4d198028dd63e22cefaad5b0c77f1a1f436d
louieBuera <isaac.277.elifer@gmail.com>
2025-04-14 14:27:16 +0800

    Add Card Component w dummy data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,21 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{query?: string}>
 }) {
   const query = (await searchParams).query;
+
+  const posts = [{
+    _createdAt: new Date(),
+    views: 55,
+    author: { _id: 1, name: 'Tempor Amet' },
+    _id: 1,
+    description: 'Adipisicing et non consequat elit velit in mollit culpa veniam et.',
+    image: 'https://images.pexels.com/photos/577514/pexels-photo-577514.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    category: 'Lighting',
+    title: 'We Flashlights'
+  }]
 
   return (
     <>
@@ -14,7 +26,17 @@ export default async function Home({ searchParams }: {
         </p>
         <SearchForm query={query}/>
       </section>
-        
+      <section className="section_container">
+        <p className="text-30 semibold">
+          { query ? `Search results for "${query}"` : 'All startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          { posts.length > 0 ? posts.map((post, index) => (
+            <StartupCard key={post._id} post={post} />
+          )) : "No Content"}
+        </ul>
+      </section>
     </>
   );
 }
